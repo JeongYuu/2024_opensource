05249eb5e17e9b20a3488ef9763efff0f4043930
JeongYuJin <wjddbwls1230@naver.com>
2024-05-13 20:56:54 +0900

    리뷰 기능 추가

---
+++ app/src/main/java/com/example/prac01/MyReviewAdapter.java
@@ -0,0 +1,60 @@
+package com.example.prac01;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MyReviewAdapter extends RecyclerView.Adapter<MyReviewAdapter.ViewHolder> {
+
+    private ArrayList<Review> reviewList;
+    private final Context context;
+
+    public MyReviewAdapter(ArrayList<Review> reviewList, Context context){
+        this.reviewList = reviewList;
+        this.context = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // Inflate your layout for each item and create a ViewHolder
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.my_review, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Review review = reviewList.get(position);
+
+        holder.menu_name.setText(review.getMenu_name());
+        holder.my_content.setText(review.getContent());
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    // Other methods and ViewHolder implementation
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        // ViewHolder implementation
+        TextView menu_name;
+        TextView my_content;
+        public ViewHolder(@NonNull View itemView){
+            super(itemView);
+            this.menu_name = itemView.findViewById(R.id.menu_name);
+            this.my_content= itemView.findViewById(R.id.my_content);
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/prac01/ReviewAdapter.java
@@ -0,0 +1,60 @@
+package com.example.prac01;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ReviewAdapter extends RecyclerView.Adapter<ReviewAdapter.ViewHolder> {
+
+    private ArrayList<Review> reviewList;
+    private final Context context;
+
+    public ReviewAdapter(ArrayList<Review> reviewList, Context context){
+        this.reviewList = reviewList;
+        this.context = context;
+    }
+
+    @Override
+    public ReviewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.personal_review, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Review review = reviewList.get(position);
+
+        holder.user_id.setText(review.getUser_id().substring(0,10)+" 님");
+        holder.content.setText(review.getContent());
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return reviewList.size();
+    }
+
+
+    // Other methods and ViewHolder implementation
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        // ViewHolder implementation
+        TextView user_id;
+        TextView content;
+        public ViewHolder(@NonNull View itemView){
+            super(itemView);
+            this.user_id = itemView.findViewById(R.id.user_id);
+            this.content= itemView.findViewById(R.id.content);
+        }
+    }
+
+
+}
